11d766beedb7d01ddaa38018e89b2efb485e0997
Debian <debian@packer-output-04d10d3b-01ac-4a13-9364-829dc0f15f63>
2022-11-15 10:28:02 +0000

    local changes

--- stats.sh
+++ stats.sh
@@ -2,4 +2,6 @@
 
 curl -s https://api.2b2t.au/v1/stats > /home/debian/old-web-data/stats.json;
 curl -s https://api.2b2t.au/v1/balance > /home/debian/old-web-data/balance.json;
+cd /home/debian/old-web-data
 ./_rebuild.sh
+echo "crontab" > test.txt
